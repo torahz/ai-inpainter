be50831f8b54918ce4bb9695182ff1de79e7ab06
torahz <rafaeltorahz@gmail.com>
2026-02-17 10:43:39 -0300

    Adjustments

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // IMPORTANTE: Se o seu repositório no GitHub se chama "meu-projeto", 
+  // a base deve ser '/meu-projeto/'. Se for o repositório principal (username.github.io), use '/'.
+  base: './', 
+})
